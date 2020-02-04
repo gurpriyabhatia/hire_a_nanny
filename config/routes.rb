407f7319a26875dd593caaee619ef36209b5e388
Gurpriya Bhatia <gurpriya.bhatia@gmail.com>
2020-02-04 19:26:05 +0000

    Updated the Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :bookings do
+    resources :reviews, only: [ :new, :create ]
+  end
+
+  resources :nannies, only: [:index, :show] do
+     resources :bookings
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
