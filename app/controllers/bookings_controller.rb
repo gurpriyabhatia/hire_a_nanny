68dea471dd621098d55e282606f073bb40e4013a
Rachel Clarke <clarkerachel21@gmail.com>
2020-02-04 19:35:45 +0000

    generated controller and entered empty actions

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,24 @@
+class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
