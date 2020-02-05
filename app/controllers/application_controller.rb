4ed7eae79cc50338ce8bd13a3d1f5a83c7f819d1
Francois Baud <fxbaud@gmail.com>
2020-02-05 16:40:45 +0000

    with the update my profile user model with the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :phone_number, :photo, :address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :phone_number, :photo, :address])
   end
 end
