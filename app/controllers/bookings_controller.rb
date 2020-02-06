3c3b1199702421661207d3125bdd94cd069f5eda
Rachel Clarke <clarkerachel21@gmail.com>
2020-02-06 19:19:38 +0000

    completed the remaining bookings actions and views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,11 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+
   def index
     @bookings = Booking.all
   end
 
   def show
-    @booking = Booking.find(params[:id])
   end
 
   def new
@@ -13,22 +14,37 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
-
+    @booking.user = current_user
+    if @booking.save!
+      redirect_to bookings_path
+    else
+      render :new
+    end
   end
-
 
   def edit
   end
 
   def update
+    if @booking.update(booking_params)
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @booking.destroy
+    redirect_to bookings_path
   end
 
   private
 
-  def booking_params
+  def set_booking
+    @booking = Booking.find(params[:id])
   end
 
+  def booking_params
+    params.require(:booking).permit(:nanny_id, :start_date, :end_date, :start_time, :address, :duration, :number_of_children)
+  end
 end
