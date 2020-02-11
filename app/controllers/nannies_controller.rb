33c42e1fc02f35ebd502e88de8caa150c15489fc
Francois Baud <fxbaud@gmail.com>
2020-02-11 09:33:31 +0000

    with address autocomplete for a new booking and a search in the map with html markers

--- app/controllers/nannies_controller.rb
+++ app/controllers/nannies_controller.rb
@@ -2,12 +2,13 @@ class NanniesController < ApplicationController
   def index
     @nannies = Nanny.where.not(latitude: nil, longitude: nil)
     @markers = @nannies.map do |nanny|
-      { lat: nanny.latitude, lng: nanny.longitude }
+      { lat: nanny.latitude, lng: nanny.longitude,
+        infoWindow: render_to_string(partial: "info_window",
+        locals: { nanny: nanny }) }
     end
   end
 
   def show
     @nanny = Nanny.find(params[:id])
   end
-
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,12 @@
 import "bootstrap";
-import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css"; // Note this is important!
 import 'mapbox-gl/dist/mapbox-gl.css';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+import flatpickr from "flatpickr";
 import { initMapbox } from './init_mapbox.js';
+import { initAutocomplete } from './init_autocomplete.js';
+
 
 flatpickr("#booking_start_date", {
   "enableTime": true
@@ -12,3 +16,4 @@ flatpickr("#booking_end_date", {
 })
 
 initMapbox();
+initAutocomplete();
