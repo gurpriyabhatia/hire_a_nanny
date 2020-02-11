d341b0559ef8f36a86e5c2b29689ea765c5d52f5
Rachel Clarke <clarkerachel21@gmail.com>
2020-02-11 21:28:26 +0000

    tweaked order in which images will appear

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,7 +46,7 @@ Nanny.create(
     phone_number: Faker::PhoneNumber.cell_phone,
     years_of_experience: Faker::Number.within(range: 0..50),
     price_per_hour: Faker::Number.within(range: 5..50),
-    image: 'nanny_1.png',
+    image: 'nanny_4.png',
     bio: Faker::Quote.most_interesting_man_in_the_world
   )
 
@@ -57,7 +57,7 @@ Nanny.create(
     phone_number: Faker::PhoneNumber.cell_phone,
     years_of_experience: Faker::Number.within(range: 0..50),
     price_per_hour: Faker::Number.within(range: 5..50),
-    image: 'nanny_2.png',
+    image: 'nanny_7.png',
     bio: Faker::Quote.most_interesting_man_in_the_world
   )
 
@@ -81,7 +81,7 @@ Nanny.create(
     phone_number: Faker::PhoneNumber.cell_phone,
     years_of_experience: Faker::Number.within(range: 0..50),
     price_per_hour: Faker::Number.within(range: 5..50),
-    image: 'nanny_4.png',
+    image: 'nanny_8.png',
     bio: Faker::Quote.most_interesting_man_in_the_world
   )
 
@@ -103,7 +103,7 @@ Nanny.create(
     phone_number: Faker::PhoneNumber.cell_phone,
     years_of_experience: Faker::Number.within(range: 0..50),
     price_per_hour: Faker::Number.within(range: 5..50),
-    image: 'nanny_6.png',
+    image: 'nanny_1.png',
     bio: Faker::Quote.most_interesting_man_in_the_world
   )
 
@@ -114,7 +114,7 @@ Nanny.create(
     phone_number: Faker::PhoneNumber.cell_phone,
     years_of_experience: Faker::Number.within(range: 0..50),
     price_per_hour: Faker::Number.within(range: 5..50),
-    image: 'nanny_7.png',
+    image: 'nanny_2.png',
     bio: Faker::Quote.most_interesting_man_in_the_world
   )
 
@@ -125,7 +125,7 @@ Nanny.create(
     phone_number: Faker::PhoneNumber.cell_phone,
     years_of_experience: Faker::Number.within(range: 0..50),
     price_per_hour: Faker::Number.within(range: 5..50),
-    image: 'nanny_8.png',
+    image: 'nanny_6.png',
     bio: Faker::Quote.most_interesting_man_in_the_world
   )
 
