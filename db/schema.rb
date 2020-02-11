150800b6b9577780f553a7102e430cee44b3c9cc
Rachel Clarke <clarkerachel21@gmail.com>
2020-02-11 20:06:43 +0000

    added image column to nanny table and populated image files for seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_112709) do
+ActiveRecord::Schema.define(version: 2020_02_11_195131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2020_02_08_112709) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "image"
   end
 
   create_table "reviews", force: :cascade do |t|
